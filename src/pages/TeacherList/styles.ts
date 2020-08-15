9b0c3c6759d1482af95c85c7292786cf29d6271b
Emilio Fernandes <emilio.fernandes-extern@renault.com>
2020-08-15 11:05:07 -0300

    Add navigation and new pages

---
+++ src/pages/TeacherList/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import styles from '../Landing/styles';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F0F0F7',
+    }
+})
+
+export default styles;
