1325b4daabd5162d5cb187443399fd5c4c5e549c
Emilio Fernandes <emilio.fernandes-extern@renault.com>
2020-08-20 16:56:14 -0300

    Fim da semana NLW2

--- src/pages/Favorites/styles.ts
+++ src/pages/Favorites/styles.ts
@@ -5,7 +5,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#F0F0F7',
-    }
+    },
+    teacherList: {
+        marginTop: -40,
+        padding: 16,
+    },
 })
 
 export default styles;
